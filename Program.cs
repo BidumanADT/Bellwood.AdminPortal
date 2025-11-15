1e98ea9f9d7988b7a831d8783e76f06b1aa2802a
BidumanADT <sgtadt@gmail.com>
2025-11-15 07:45:04 -0600

    Initial Commit
    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+// HTTP Client for calling AdminAPI
+builder.Services.AddHttpClient("AdminAPI", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5007/");
+    client.Timeout = TimeSpan.FromSeconds(30);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
